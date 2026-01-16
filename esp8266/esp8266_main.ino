619af44cb80516fb2118f9fd9be3615b1a596276
Zekeriya Kalkan <153025313+zekeriyakalkann@users.noreply.github.com>
2026-01-16 17:43:39 +0300

    Refactor ESP8266 code into modular notification system

--- esp8266/esp8266_main.ino
+++ esp8266/esp8266_main.ino
@@ -1,1 +1,25 @@
-// ESP8266 communication module
+#include "secrets.h"
+#include "parser.h"
+#include "ifttt.h"
+#include <ESP8266WiFi.h>
+
+void setup() {
+  Serial.begin(9600);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String line = Serial.readStringUntil('\n');
+    AlarmPayload p = parseLine(line);
+
+    if (p.isAlarm) {
+      sendIfttt(p.temperature, p.humidity);
+      delay(5000);
+    }
+  }
+}
