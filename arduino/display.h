353401640584c920c5c77df22e50063553c66798
Zekeriya Kalkan <153025313+zekeriyakalkann@users.noreply.github.com>
2026-01-16 17:33:28 +0300

    Create display.h

---
+++ arduino/display.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <Arduino.h>
+
+void displayInit();
+void displayShowTopLine(int temperature, int humidity);
+void displayShowStatusClean(int gas);
+void displayShowStatusWarning();
+void displayShowStatusAlarm();
