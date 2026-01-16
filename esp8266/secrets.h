06c4c666ffce8ac7ebf29357056f81a9ef3a22b3
Zekeriya Kalkan <153025313+zekeriyakalkann@users.noreply.github.com>
2026-01-16 17:40:05 +0300

    Add WiFi and IFTTT configuration file

---
+++ esp8266/secrets.h
@@ -0,0 +1,8 @@
+#pragma once
+
+const char* WIFI_SSID = "Zekeriya Wifi";
+const char* WIFI_PASS = "12345678";
+
+const char* IFTTT_KEY   = "PUT_YOUR_KEY_HERE";
+//I'm keeping the key secret; it can be added during the activation process.
+const char* IFTTT_EVENT = "gaz_alarm";
